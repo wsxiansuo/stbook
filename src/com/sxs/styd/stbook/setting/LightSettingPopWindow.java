e2e356a6e85accf77b47a57228f43020845d736a
wsxiansuo <wsxiansuo@gmail.com>
2014-12-10 17:56:02 +0800

    xiugai

--- src/com/sxs/styd/stbook/setting/LightSettingPopWindow.java
+++ src/com/sxs/styd/stbook/setting/LightSettingPopWindow.java
@@ -2,6 +2,11 @@ package com.sxs.styd.stbook.setting;
 
 import android.widget.PopupWindow;
 
+/**
+ * ÁÁ¶Èµ÷½Úpopwindow
+ * @author user
+ *
+ */
 public class LightSettingPopWindow extends PopupWindow {
 
 }
